43c1209d01999fce95db5dddb0f2a6ccae9e9948
Nazarkulov Aman <aman.nazarkulov@gmail.com>
2025-03-14 20:03:56 +0600

    Adding a dictionary for English characters

--- MorseTranslator/Program.cs
+++ MorseTranslator/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("... --- ...");
+class MorseTranslator {
+    private static Dictionary<char, string> _morseMap = new Dictionary<char, string> {
+        {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
+        {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
+        {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
+        {'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."},
+        {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+        {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
+        {'Y', "-.--"}, {'Z', "--.."}, {' ', "/"}
+    };
+
+    public static void Main(string[] args) { 
+        string input = "hello world !3 asd";
+        string output = "";
+        foreach (char c in input.ToUpper()) {
+            output += _morseMap.ContainsKey(c) ? _morseMap[c] + " " : "incorrect symbol ";
+        }
+        Console.WriteLine(output.Trim());
         Console.ReadKey();
     }
 }
