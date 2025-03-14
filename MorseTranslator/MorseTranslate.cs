91dc0dfd551b931f729dadd901a079b0a7dc5603
Nazarkulov Aman <aman.nazarkulov@gmail.com>
2025-03-14 23:52:39 +0600

    Adding russian language support (v0.2)

--- MorseTranslator/MorseTranslate.cs
+++ MorseTranslator/MorseTranslate.cs
@@ -4,7 +4,7 @@
     using System.Collections.Generic;
 
     public class MorseTranslator {
-        static Dictionary<char, string> _morseMap = new Dictionary<char, string> {
+        private static Dictionary<char, string> _englishMap = new Dictionary<char, string> {
             {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
             {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
             {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
@@ -13,11 +13,34 @@
             {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
             {'Y', "-.--"}, {'Z', "--.."}, {' ', "/"}
         };
+        
+        private static Dictionary<char, string> _russianMap = new Dictionary<char, string> {
+            {'А', ".-"}, {'Б', "-..."}, {'В', ".--"}, {'Г', "--."}, {'Д', "-.."},
+            {'Е', "."}, {'Ж', "...-"}, {'З', "--.."}, {'И', ".."}, {'Й', ".---"},
+            {'К', "-.-"}, {'Л', ".-.."}, {'М', "--"}, {'Н', "-."}, {'О', "---"},
+            {'П', ".--."}, {'Р', ".-."}, {'С', "..."}, {'Т', "-"}, {'У', "..-"},
+            {'Ф', "..-."}, {'Х', "...."}, {'Ц', "-.-."}, {'Ч', "---."}, {'Ш', "----"},
+            {'Щ', "--.-"}, {'Ы', "-.--"}, {'Э', "..-.."}, {'Ю', "..--"}, {'Я', ".-.-"},
+            {' ', "/"}
+        };
 
-        private static string Translate(string text) {
+        private static string Translate(string text, string version) {
             string output = "";
-            foreach (char c in text.ToUpper()) {
-                output += _morseMap.ContainsKey(c) ? _morseMap[c] + " " : "incorrect character ";
+            string upper = text.ToUpper();
+            if (version == "v0.2" || version == "v1.0") {
+                foreach (char c in upper) {
+                    if (_russianMap.ContainsKey(c))
+                        output += _russianMap[c] + " ";
+                    else if (_englishMap.ContainsKey(c))
+                        output += _englishMap[c] + " ";
+                    else
+                        output += "incorrect character ";
+                }
+            }
+            else {
+                foreach (char c in upper) {
+                    output += _englishMap.ContainsKey(c) ? _englishMap[c] + " " : "incorrect character ";
+                }
             }
             return output.Trim();
         }
@@ -34,7 +57,9 @@
             }
 
             string inputText = fileManager.Read(inputPath);
-            string morse = Translate(inputText);
+            Console.WriteLine("Enter the version (v0.1, v0.2, v1.0):");
+            string version = Console.ReadLine();
+            string morse = Translate(inputText, version);
             fileManager.Write(outputPath, morse);
             Console.WriteLine("Result recorded in " + outputPath);
             Console.ReadKey();
